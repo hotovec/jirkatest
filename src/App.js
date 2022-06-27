d2ec26b624461c927ceb11b05ce5411898390cd7
Michal Hotovec <michal.hotovec@gmail.com>
2022-06-27 17:55:20 +0200

    prvni pyčo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {Button, Dropdown, Paragraph} from '@george-labs.com/design-system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+
+          <h1>Kunda H1</h1>
+          <Button>Pyčo</Button>
+          <Button className='ml-3'
+                  icon={Button.ICON.APP_LIST}
+                  hiddenLabel={Button.HIDDEN_LABEL.BELOW_MD}
+                  variant={Button.VARIANT.DANGER}
+          >Pyčo s ikonem</Button>
+          <Dropdown className='ml-3'
+                    renderToggle={() =>
+                      <Button>Pyčo s blikajicim dropdonem</Button>
+                    }>
+            <Paragraph>Kuk pyčo, blik pyčo</Paragraph>
+          </Dropdown>
+
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { LanguageProvider, ThemeProvider } from '@george-labs.com/design-system';
+import 'react-dates/lib/css/_datepicker.css'; // Optional if you want to use DateInput/DateRangeInput
+import '@george-labs.com/design-system/dist/bootstrap.min.css';
+import '@george-labs.com/design-system/dist/gds-main.min.css'; // or gds-store.min.css
+import '@george-labs.com/design-system/dist/gds-components.min.css';
+
+ReactDOM.render(
+   <React.StrictMode>
+    <ThemeProvider theme={ThemeProvider.THEME.BS4}>
+     <App />
+    </ThemeProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
+ );
